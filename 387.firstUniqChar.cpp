ad7e5242d64212793b87cf01b9905af975402cd4
ChanceyLi <51819750+ChanceyLi@users.noreply.github.com>
2020-12-23 09:58:43 +0800

    Update 387.firstUniqChar.cpp

--- 387.firstUniqChar.cpp
+++ 387.firstUniqChar.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         vector<pair<int,int>> tmp(26,{0,0});
-        for(int i = 0; i < s.length(); ++i) {
-            if(tmp[s[i] - 'a'].first == 0)
-                tmp[s[i] - 'a'].first = i + 1;
-            else tmp[s[i] - 'a'].second = 1;
-        }
+        for(int i = 0; i < s.length(); ++i) 
+            if(!tmp[s[i] - 'a'].first)
+                tmp[s[i] - 'a'].first = i + 1;//first用来记录第一个字母出现的位置(+1，避免为0)
+            else tmp[s[i] - 'a'].second = 1;//second用来记录是否重复
         int res = INT_MAX;
         for(int i = 0; i < 26; ++i)
-            if(!tmp[i].second)
+            if(!tmp[i].second && tmp[i].first)//如果不重复且字母出现
                 res = min(res,tmp[i].first);
-        return res - 1;
+        if(res == INT_MAX) return -1;//如果res值不改变，返回-1
+        return (res - 1);
     }
 };
