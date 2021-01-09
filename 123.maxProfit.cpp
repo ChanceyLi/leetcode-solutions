a7565c3056f3f6aeae01845884b350f64b8dadac
ChanceyLi <51819750+ChanceyLi@users.noreply.github.com>
2021-01-09 09:54:46 +0800

    Update 123.maxProfit.cpp

--- 123.maxProfit.cpp
+++ 123.maxProfit.cpp
@@ -11,3 +11,15 @@ public:
         return sell[2];
     }
 };
+/*
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        INT_MIN = -2147483648
+        sell=[0,0,0]
+        buy =[INT_MIN,INT_MIN,INT_MIN]
+        for ele in prices:
+            for i in range(1,3,1):
+                buy[i] = max(buy[i], sell[i - 1] - ele)
+                sell[i] = max(sell[i], buy[i] + ele)
+        return sell[2]
+*/
