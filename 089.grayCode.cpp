bae7df266dbac673c693e06289a3ba897e3c1ad4
ChanceyLi <51819750+ChanceyLi@users.noreply.github.com>
2021-01-23 17:31:16 +0800

    Create 089.grayCode.cpp

---
+++ 089.grayCode.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> gray;
+        for(int i = 0; i < 1 << n; ++i) 
+            gray.push_back(i ^ i >> 1);
+        return gray;
+    }
+};
